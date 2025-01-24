b09f28c79d6d79925cb6d3a2b12b77d8f48640e0
Pradhumen <pradhumensingh1112@gmail.com>
2025-01-24 14:26:47 +0530

    update image2

--- src/app/components/ImageCard.js
+++ src/app/components/ImageCard.js
@@ -1,42 +1,50 @@
-import { Box, Card, CardActions, CardContent, Grid, Typography } from '@mui/material';
-import Image from 'next/image';
-import React from 'react';
+import {
+  Box,
+  Card,
+  CardActions,
+  CardContent,
+  Grid,
+  Typography,
+} from "@mui/material";
+import Image from "next/image";
+import React from "react";
 
 import typography, {
-    Helvetica_Neue,
-    Helvetica_Neue_Regular,
-    Helvetica_Neue_Thin,
-    Helvetica_Neue_Light,
-    Helvetica_Neue_Medium,
-    Helvetica_Neue_Bold,
-    Helvetica_Neue_Light_Arabic,
-    Helvetica_Neue_Bold_Arabic,
-    Helvetica_Neue_Regular_Arabic,
-    Helvetica_Neue_Thin_Arabic,
-    Helvetica_Neue_Medium_Arabic,
-    Helvetica_Neue_Arabic,
-  } from "../../theme/typography";
+  Helvetica_Neue,
+  Helvetica_Neue_Regular,
+  Helvetica_Neue_Thin,
+  Helvetica_Neue_Light,
+  Helvetica_Neue_Medium,
+  Helvetica_Neue_Bold,
+  Helvetica_Neue_Light_Arabic,
+  Helvetica_Neue_Bold_Arabic,
+  Helvetica_Neue_Regular_Arabic,
+  Helvetica_Neue_Thin_Arabic,
+  Helvetica_Neue_Medium_Arabic,
+  Helvetica_Neue_Arabic,
+} from "../../theme/typography";
 
-const ImageCard = ({ functionname, index, img,refName,category,name }) => {
+const ImageCard = ({ functionname, index, img, refName, category, name }) => {
   return (
-    <Grid xs={6} sm={6} md={4} item key={index} sx={{mt:2}}>
-      
+    <Grid xs={6} sm={6} md={4} item key={index} sx={{ mt: 2 }}>
       <Card
-        
         ref={refName}
-        onClick={() => functionname(index)}  
+        onClick={() => functionname(index)}
         sx={{
-          
           boxShadow:
-           category === index
-              ? "0 0 0 3px #ef9c00"
-              : "0 0 0 2px #ededec",
+            category === index ? "0 0 0 3px #ef9c00" : "0 0 0 2px #ededec",
           borderRadius: "10px",
+          width: "100%", 
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
           "&:hover": {
             boxShadow:
-            category === index
-                ? "0 0 0 3px #ef9c00"
-                : "0 0 0 2px #56504b",
+              category === index ? "0 0 0 3px #ef9c00" : "0 0 0 2px #56504b",
+          },
+          "@media (max-width: 768px)": {
+            // For tablets
+            width: "100%", 
           },
         }}
       >
@@ -46,41 +54,43 @@ const ImageCard = ({ functionname, index, img,refName,category,name }) => {
             flexDirection: "column",
             alignItems: "center",
             justifyContent: "center",
-            pt:"0"
+            pt: "0",
+            width: "100%",
           }}
         >
-          <Box>
-       
+          <Box
+            sx={{
+              width: "100%", 
+              maxWidth: 300, 
+            }}
+          >
             <Image
               src={img}
-              width={200}
-              height={150}
+              layout="responsive"
+              width={200} 
+              height={150} 
               alt="Picture of the author"
               style={{
-                objectFit: "cover",
+                objectFit: "cover", 
               }}
             />
-          
           </Box>
 
           <CardActions
             sx={{
               display: "flex",
               justifyContent: "center",
-              // paddingTop: 1,
+              width: "100%", 
             }}
           >
             <Typography
-              
               sx={{
                 fontFamily: Helvetica_Neue_Bold.style.fontFamily,
                 fontWeight: 800,
                 color: "#686058",
                 textAlign: "center",
                 fontSize: "small",
-                // whiteSpace: "normal",
-                // overflow: "hidden",
-                // textOverflow: "ellipsis",
+                wordWrap: "break-word", 
               }}
               gutterBottom
               variant="h6"
